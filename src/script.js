bcdccc841078a6bde54f6b9b36ef0909604281e4
mdfaizan0 <faizanm78692@gmail.com>
2025-04-07 14:14:42 +0530

    Added and tested API call to OpenWeatherMap endpoint

--- src/script.js
+++ src/script.js
@@ -1,24 +1,22 @@
-// let btn = document.querySelector("#btn")
-// let displayDiv = document.querySelector("#displayDiv")
+let btn = document.querySelector(".github-logo")
 
-// btn.addEventListener("click", () => {
-//     getData()
-// })
+btn.addEventListener("click", () => {
+    getData()
+})
 
-// async function getData() {
-//     try {
-//         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=***REMOVED***&units=metric`)
-//         if (!response.ok) {
-//             console.log(`${response.status}: ${(await response).statusText}`)
-//         }
-//         let data = await response.json()
-//         // console.log(data)
-//         console.log(new Date(data.dt * 1000))
-//         displayDetails(data)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+async function getData() {
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=***REMOVED***&units=metric`)
+        if (!response.ok) {
+            console.log(`${response.status}: ${response.statusText}`)
+        }
+        let data = await response.json()
+        // console.log(data)
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 // function displayDetails(data) {
 //     let p = document.createElement("p")
