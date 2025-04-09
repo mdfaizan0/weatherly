d342400a67355edd99960a53408d8f473a8e6629
mdfaizan0 <faizanm78692@gmail.com>
2025-04-09 08:22:33 +0530

    Added location-pin aside input and some other visual aesthetic improvements:
    - Added favicon
    - Updated weather logo
    - Updated Font for header and body

--- src/script.js
+++ src/script.js
@@ -7,21 +7,28 @@ btn.addEventListener("click", () => {
 async function getData() {
     try {
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=***REMOVED***&units=metric`)
+        // let responseForecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=kolkata&appid=***REMOVED***&units=metric`)
         if (!response.ok) {
             console.log(`${response.status}: ${response.statusText}`)
         }
         let data = await response.json()
-        // console.log(data)
         console.log(data)
+        displayTodayDetails(data)
     } catch (error) {
         console.log(error)
     }
 }
 
-// function displayDetails(data) {
-//     let p = document.createElement("p")
-//     p.innerHTML = data.main.temp
-//     console.log(data)
+function displayTodayDetails(data) {
+    let cityName = data.name
+    let country = data.sys.country
+    let date = new Date(data.dt)
+    let temperature = Math.round(data.main.temp)
+    let weatherDescrip = data.weather.description
+    let feelsLike = data.main.feels_like
+    let windSpeed = data.wind.speed
+    let clouds = data.clouds.all
+    let humidity = data.main.humidity
 
-//     displayDiv.appendChild(p)
-// }
+    
+}
